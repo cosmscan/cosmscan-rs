e9144c0349493df16467fea5f79c8817358f113b
cailhuiris <cailhuiris@gmail.com>
2022-07-26 16:51:56 +0900

    feat: added tendermint client

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,13 +1,12 @@
+use tendermint::block::Height;
+use tendermint_rpc::{Client, HttpClient};
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let body = reqwest::get("http://localhost:26657/block?height=1")
-        .await?
-        .text()
-        .await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = HttpClient::new("http://localhost:26657").unwrap();
+    let block = client.block(Height::from(1u32)).await.unwrap();
 
-    println!("body = {:?}", body);
-
-    println!("Hello, world!");
+    println!("block height at 1 =  {:?}", block);
 
     Ok(())
 }
