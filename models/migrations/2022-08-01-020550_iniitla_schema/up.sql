1c5a0c19945a72db87e6bdc57f769904fbe0c89d
scalalang2 <cailhuiris@gmail.com>
2022-08-24 22:50:49 +0900

    feat: add more schemas

--- models/migrations/2022-08-01-020550_iniitla_schema/up.sql
+++ models/migrations/2022-08-01-020550_iniitla_schema/up.sql
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS blocks (
     inserted_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP
 );
+CREATE INDEX idx_blocks_chain_id ON blocks(chain_id);
 CREATE INDEX idx_blocks_height ON blocks(height);
 CREATE INDEX idx_blocks_block_hash ON blocks(block_hash);
 
@@ -52,6 +53,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     inserted_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP
 );
+CREATE INDEX idx_transactions_chain_id ON events(chain_id);
 CREATE INDEX idx_transactions_tx_hash ON transactions(transaction_hash);
 
 -- events --
@@ -59,11 +61,44 @@ CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
     chain_id INT NOT NULL,
     tx_type SMALLINT NOT NULL, -- 0: transaction, 1: begin_block, 2: after_block
-    tx_hash VARCHAR(256), -- it can be null
+    tx_hash VARCHAR(256),      -- it can be null
     event_type VARCHAR(256) NOT NULL,
     event_key VARCHAR(256) NOT NULL,
     event_value VARCHAR(256) NOT NULL,
     indexed BOOLEAN NOT NULL,
     inserted_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP
+);
+CREATE INDEX idx_events_chain_id ON events(chain_id);
+CREATE INDEX idx_events_tx_hash ON events(tx_hash);
+
+-- account --
+CREATE TABLE IF NOT EXISTS accounts (
+    id SERIAL PRIMARY KEY,
+    chain_id INT NOT NULL,
+    address VARCHAR(256) UNIQUE NOT NULL,
+    inserted_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE INDEX idx_accounts_chain_id ON accounts(chain_id);
+
+-- account_balance --
+CREATE TABLE IF NOT EXISTS account_balance (
+    id SERIAL PRIMARY KEY,
+    account_id INT NOT NULL,
+    amount BIGINT NOT NULL,
+    denom VARCHAR(20) NOT NULL,
+    inserted_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_account_id FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+-- messages --
+CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    transaction_id INT NOT NULL,
+    messages JSONB,
+    inserted_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_transaction_id FOREIGN KEY (transaction_id) REFERENCES transactions(id)
 );
